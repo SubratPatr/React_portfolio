1dda8b11c81fa04cd2fca50c7703879f71897da2
Subrata Kumar Patra <124023377+SubratPatr@users.noreply.github.com>
2024-03-26 04:31:08 +0530

    Add files via upload

---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+import styles from "./Contact.module.css";
+import { getImageUrl } from "../../utils";
+
+export const Contact = () => {
+  return (
+    <footer id="contact" className={styles.container}>
+      <div className={styles.text}>
+        <h2>Contact</h2>
+        <p>Feel free to reach out!</p>
+      </div>
+      <ul className={styles.links}>
+        <li className={styles.link}>
+          <img src={getImageUrl("contact/emailIcon.png")} alt="Email icon" />
+          <a href="mailto:patrasubrat894@gmail.com">patrasubrat894@gmail.com</a>
+        </li>
+        <li className={styles.link}>
+          <img
+            src={getImageUrl("contact/linkedinIcon.png")}
+            alt="LinkedIn icon"
+          />
+          <a href="https://www.linkedin.com/in/subrat-kumar-patra-a39297266">linkedin.com/in/subrat-kumar-patra</a>
+        </li>
+        <li className={styles.link}>
+          <img src={getImageUrl("contact/githubIcon.png")} alt="Github icon" />
+          <a href="https://github.com/SubratPatr">github.com/SubratPatra</a>
+        </li>
+      </ul>
+    </footer>
+  );
+};
